f6708875d9cd443c9912c6ad4687b8692259a994
Frank Hilgenberg <frank.hilgenberg@franks-divecenter.de>
2024-03-30 16:17:13 +0100

    Kleine Schönheitskorekturen

--- src/entitaeten/Zeitschrift.java
+++ src/entitaeten/Zeitschrift.java
@@ -6,28 +6,33 @@ public class Zeitschrift extends Medium {
 	
 	private static int bestand = 0;
 	
-	// Konstruktor für die Klasse zur Initialisierung
-	// der Datenfelder
-	public Zeitschrift (String titel, String unterTitel, String issn, int anzahlExemplare) {
+	// Konstruktor für die Klasse zur Initialisierung der Datenfelder
+	public Zeitschrift (String titel, String unterTitel, String issn, int anzahlExemplare)
+	{
 		super(titel, unterTitel, issn, anzahlExemplare);
-		// Bei der Objekterzeugung wird die Klassenvariable
-		// bestand um eins erhöht
+		// Bei der Objekterzeugung wird die Klassenvariable bestand um eins erhöht
 		bestand++;
 	}
+	
 	// Zweiter Konstruktor mit 3 Parametern ohne Untertitel
-	public Zeitschrift (String titel, String issn, int anzahlExemplare) {
+	public Zeitschrift (String titel, String issn, int anzahlExemplare)
+	{
 		this(titel, "", issn, anzahlExemplare);
 	}
 			
-//	public void ausgeben() {	
-//		System.out.println("[Zeitschrift]" + titel + " mit ISSN: " + mediumNummer + ", Bestand: " + anzahlExemplare);
-//	}
+	//public void ausgeben()
+	//{	
+	//System.out.println("[Zeitschrift]" + titel + " mit ISSN: " + mediumNummer + ", Bestand: " + anzahlExemplare);
+	//}
 	
-	public static int getbestand() {
+	public static int getbestand()
+	{
 		return bestand;
 	}
 	
-	public String toString() {
+	// Überschreibt entitaeten.Medium.toString
+	public String toString()
+	{
 		return "[Zeitschrift]: " + super.toString();
 	}
 }
